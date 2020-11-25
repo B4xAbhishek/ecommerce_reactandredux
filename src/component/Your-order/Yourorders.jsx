88155fe555443599c3bfb57480c7988b1068c678
Abhishek kumar verma <50478316+B4xAbhishek@users.noreply.github.com>
2020-11-25 23:51:07 +0530

    ecommerce react+redux

---
+++ src/component/Your-order/Yourorders.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link, useHistory } from "react-router-dom";
+
+function Yourorders() {
+    return (
+        <div>
+            <h2>Payment Succesfull</h2>
+            <Link to="/">  <button>Go to Home page</button> </Link>
+        </div>
+    )
+}
+
+export default Yourorders
---
+++ src/pages/Yourorders/Yourorders.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Yourorders() {
+    return (
+        <div>
+            <h2>loading</h2>
+        </div>
+    )
+}
+
+export default Yourorders
